2e3beb570b18b5f56322553c316895e06d4d4804
Chintan Ramila Mistry <98411347+ChintanMstry@users.noreply.github.com>
2023-02-01 12:43:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-var connect = require('connect');
- var serveStatic = require('serve-static');
+const express = require("express");
+var cors = require('cors')
+const port = 80;
+const app = express();
+app.get("/products",  cors(), (req, resp) => {
+    //resp.send("working");
+    resp.json([{ "Name": "Bats", "Age": "17", "Game": "Cricket" }, { "Name": "Sticks", "Age": "2", "Game": "Hockey"},  { "Name": "Bicycle", "Age": "12", "Game": "Racing"}]);
+  });
+app.get("/",  cors(), (req, resp) => {
+    //resp.send("working");
+    resp.json([{ "Name": "AtishubhV", "Age": "35", "Game": "Footballss" }, { "Name": "Mark", "Age": "32", "Game": "VolleyBall"}]);
+  });
 
- connect()
-     .use(serveStatic(__dirname))
-     .listen(8083, () => console.log('Server running on 8080...'));
+app.listen(port, console.log(`Server started at port ${port}`));
