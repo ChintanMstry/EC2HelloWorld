8d47b782f1bcdb261b82107f47d617b76ba87786
Chintan Ramila Mistry <98411347+ChintanMstry@users.noreply.github.com>
2023-02-01 13:53:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require("express");
 var cors = require('cors')
-const port = 80;
+const port = 8083;
 const app = express();
-app.get("/products",  cors(), (req, resp) => {
-    //resp.send("working");
-    resp.json([{ "Name": "Bats", "Age": "17", "Game": "Cricket" }, { "Name": "Sticks", "Age": "2", "Game": "Hockey"},  { "Name": "Bicycle", "Age": "12", "Game": "Racing"}]);
-  });
+
 app.get("/",  cors(), (req, resp) => {
     //resp.send("working");
     resp.json([{ "Name": "AtishubhV", "Age": "35", "Game": "Footballss" }, { "Name": "Mark", "Age": "32", "Game": "VolleyBall"}]);
